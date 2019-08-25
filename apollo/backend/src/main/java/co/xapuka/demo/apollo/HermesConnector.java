893997441cb33193bbb209f662f2e14ad925fc76
juan-amorocho <jamoroch@gmail.com>
2019-08-25 18:42:01 +0200

    Replace logic of HermesConnector with real resttemplate

--- apollo/backend/src/main/java/co/xapuka/demo/apollo/HermesConnector.java
+++ apollo/backend/src/main/java/co/xapuka/demo/apollo/HermesConnector.java
@@ -2,26 +2,38 @@ package co.xapuka.demo.apollo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class HermesConnector {
+
     private static Logger LOG = LoggerFactory.getLogger(HermesConnector.class);
+
+    @Value("${apollo.hermes-base-url:http://localhost:1000}")
+    private String baseUrl;
+
+    private RestTemplate client = new RestTemplate();
+
     public void send(Map<String, String> payload, JobType type) {
 
-        if(JobType.CREATE_COMPANY.equals(type)) {
-            RuntimeException e = new RuntimeException("For test purposes! Throwing on CREATED_COMPANY" );
-            LOG.warn("For test purpose only! CREATE_COMPANY jobs lead to this exception");
-            throw e;
+
+        switch (type) {
+            case CREATE_COMPANY:
+                client.postForLocation(baseUrl, payload, Map.class);
+
+                break;
+            case UPDATE_COMPANY:
+                String companyId = payload.get("companyId");
+                String tags = payload.get("tags");
+                client.put(String.format("%s/%s/%s", baseUrl, companyId, tags), null);
+                break;
         }
 
-        String result = payload.entrySet()
-                .stream()
-                .map(entry -> entry.getKey() + " = " + entry.getValue())
-                .collect(Collectors.joining(" | "));
-        LOG.info("Job Type " + type + "payload" +result);
     }
 }
